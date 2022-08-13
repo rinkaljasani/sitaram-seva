7d231bcfb0698cf293aa358b2608606aceffe25a
Rinkal Jasani <rinkal.j@yudiz.com>
2022-08-13 09:16:07 +0530

    css path change

--- resources/views/admin/layouts/includes/css.blade.php
+++ resources/views/admin/layouts/includes/css.blade.php
@@ -1,16 +1,16 @@
 {{-- Global Theme Styles(used by all pages) Start --}}
 {{-- <link href="{{ asset('admin/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" /> --}}
 <link href="{{ asset('admin/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/custom/prismjs/prismjs.bundle.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.bundle.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/plugins/global/plugins.bundle.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/plugins/custom/prismjs/prismjs.bundle.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/style.bundle.css') }}" />
 {{-- Global Theme Styles(used by all pages) End --}}
 
 {{-- Layout Themes(used by all pages) Start --}}
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themes/layout/header/base/light.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themes/layout/header/menu/light.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themes/layout/brand/dark.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themes/layout/aside/dark.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/themes/layout/header/base/light.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/themes/layout/header/menu/light.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/themes/layout/brand/dark.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/themes/layout/aside/dark.css') }}" />
 {{-- Layout Themes(used by all pages) End --}}
 
 <link href="{{ asset('admin/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
